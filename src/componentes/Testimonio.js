9718c86db1bda68f95f7d440e16e6ec7f1d78e02
JorgeBaron92 <jlbm1992@gmail.com>
2025-01-30 21:46:02 -0600

    version inicial

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Testimonio(){
+  return (
+    <div className="contendor-testimonio">
+      <img className="imagen-testimonio"
+      src={require("../imagenes/1099445.webp")}
+      alt="imagen de Kimetsu"/>
+      
+
+    <div className="contenedor-texto-textimonio">
+      <p  className="nombre-testimonio">Tanjiro</p>
+      <p  className="cargo-testimonio">Protagonista</p>
+      <p  className="texto-testimonio">jajajajajajajajajaajjajajajajaja</p>
+    </div>
+    
+    </div>
+
+    
+  )
+}
+export default Testimonio;
